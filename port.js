2f4d1d4647833046d9600d6464666576bc5f3497
kanica312 <10kanica10aalex@gmail.com>
2025-04-19 18:26:24 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,30 @@
+let modeBtn=document.querySelector("#mode");
+
+let currMode="dark";
+
+
+modeBtn.addEventListener("click",()=>{
+    
+if(currMode==="dark"){
+    
+currMode="light";
+    
+
+ document.querySelector("body").style.backgroundImage="url('https://tse1.mm.bing.net/th?id=OIP.bhQ1UqG4bTO5hOmhrdM51gHaE7&pid=Api&P=0&h=180')"; 
+    modeBtn.innerHTML="Dark Mode";
+   
+ 
+}
+
+else{
+  
+  currMode="dark";
+   
+ document.querySelector("body").style.backgroundImage="url('https://tse3.mm.bing.net/th?id=OIP.snOCo7y8gBUb7r0Qw9OJMwHaEK&pid=Api&P=0&h=180')";
+
+  
+ 
+ modeBtn.innerHTML="Light Mode";
+
+}
+});
